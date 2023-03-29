66caa8b61231fd5db549ba89c4574fe4856d794c
Ben Bartling <ben.bartling@gmail.com>
2023-03-29 08:34:10 -0500

    all unit tests done!

--- tests/unit/test_ahu_fc11.py
+++ tests/unit/test_ahu_fc11.py
@@ -1,28 +1,29 @@
-from faults import FaultConditionTen, HelperUtils
+from faults import FaultConditionEleven, HelperUtils
 import pandas as pd
 import pytest
 
 '''
 to see print statements in pytest run with
-$ pytest tests/unit/test_ahu_fc10.py -rP
+$ pytest tests/unit/test_ahu_fc11.py -rP
 
-SAT and MAT should be approx equal in OS3
+OAT Temp too low for 100% cooling in OS3
 '''
 
-
+TEST_DELTA_SUPPLY_FAN = 2
 TEST_OAT_DEGF_ERR_THRES = 5
-TEST_MIX_DEGF_ERR_THRES = 2
+TEST_SUPPLY_DEGF_ERR_THRES = 2
+TEST_SAT_SP_COL = "supply_air_sp_temp"
 TEST_OAT_COL = "out_air_temp"
-TEST_MAT_COL = "mix_air_temp"
 TEST_COOLING_COIL_SIG_COL = "cooling_sig_col"
 TEST_MIX_AIR_DAMPER_COL = "economizer_sig_col"
 
 
-fc10 = FaultConditionTen(
+fc11 = FaultConditionEleven(
+    TEST_DELTA_SUPPLY_FAN,
     TEST_OAT_DEGF_ERR_THRES,
-    TEST_MIX_DEGF_ERR_THRES,
+    TEST_SUPPLY_DEGF_ERR_THRES,
+    TEST_SAT_SP_COL,
     TEST_OAT_COL,
-    TEST_MAT_COL,
     TEST_COOLING_COIL_SIG_COL,
     TEST_MIX_AIR_DAMPER_COL,
 )
@@ -32,7 +33,7 @@ class TestNoFaultNoEcon(object):
 
     def no_fault_df_no_econ(self) -> pd.DataFrame:
         data = {
-            TEST_MAT_COL: [55],
+            TEST_SAT_SP_COL: [55],
             TEST_OAT_COL: [56],
             TEST_COOLING_COIL_SIG_COL: [.11],
             TEST_MIX_AIR_DAMPER_COL: [.99],
@@ -40,10 +41,10 @@ class TestNoFaultNoEcon(object):
         return pd.DataFrame(data)
 
     def test_no_fault_no_econ(self):
-        results = fc10.apply(self.no_fault_df_no_econ())
-        actual = results.loc[0, 'fc10_flag']
+        results = fc11.apply(self.no_fault_df_no_econ())
+        actual = results.loc[0, 'fc11_flag']
         expected = 0.0
-        message = f"fc10 no_fault_df_no_econ actual is {actual} and expected is {expected}"
+        message = f"fc11 no_fault_df_no_econ actual is {actual} and expected is {expected}"
         assert actual == expected, message
 
 
@@ -51,18 +52,18 @@ class TestFaultInecon(object):
 
     def fault_df_in_econ(self) -> pd.DataFrame:
         data = {
-            TEST_MAT_COL: [55],
-            TEST_OAT_COL: [75],
+            TEST_SAT_SP_COL: [55],
+            TEST_OAT_COL: [44],
             TEST_COOLING_COIL_SIG_COL: [.11],
             TEST_MIX_AIR_DAMPER_COL: [.99],
         }
         return pd.DataFrame(data)
 
     def test_fault_in_econ(self):
-        results = fc10.apply(self.fault_df_in_econ())
-        actual = results.loc[0, 'fc10_flag']
+        results = fc11.apply(self.fault_df_in_econ())
+        actual = results.loc[0, 'fc11_flag']
         expected = 1.0
-        message = f"fc10 fault_df_in_econ actual is {actual} and expected is {expected}"
+        message = f"fc11 fault_df_in_econ actual is {actual} and expected is {expected}"
         assert actual == expected, message
 
 
@@ -71,7 +72,7 @@ class TestFaultOnInt(object):
 
     def fault_df_on_output_int(self) -> pd.DataFrame:
         data = {
-            TEST_MAT_COL: [55],
+            TEST_SAT_SP_COL: [55],
             TEST_OAT_COL: [75],
             TEST_COOLING_COIL_SIG_COL: [11],
             TEST_MIX_AIR_DAMPER_COL: [.80],
@@ -81,14 +82,14 @@ class TestFaultOnInt(object):
     def test_fault_on_int(self):
         with pytest.raises(TypeError, 
                            match=HelperUtils().float_int_check_err(TEST_COOLING_COIL_SIG_COL)):
-            fc10.apply(self.fault_df_on_output_int())
+            fc11.apply(self.fault_df_on_output_int())
 
 
 class TestFaultOnFloatGreaterThanOne(object):
 
     def fault_df_on_output_greater_than_one(self) -> pd.DataFrame:
         data = {
-            TEST_MAT_COL: [55],
+            TEST_SAT_SP_COL: [55],
             TEST_OAT_COL: [75],
             TEST_COOLING_COIL_SIG_COL: [11.0],
             TEST_MIX_AIR_DAMPER_COL: [.80],
@@ -98,5 +99,5 @@ class TestFaultOnFloatGreaterThanOne(object):
     def test_fault_on_float_greater_than_one(self):
         with pytest.raises(TypeError,
                            match=HelperUtils().float_max_check_err(TEST_COOLING_COIL_SIG_COL)):
-            fc10.apply(self.fault_df_on_output_greater_than_one())
+            fc11.apply(self.fault_df_on_output_greater_than_one())
             
